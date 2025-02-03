38176647488b7b3f5abdee8d42a726aaa20236b9
rickrick3 <rickrick20050@gmail.com>
2025-02-03 08:56:01 -0800

    frontend vercel

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ const App = () => {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<AdminLogin />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/admin-register" element={<AdminRegistration />} />
           <Route path="/tickets" element={<TicketList />} />
           <Route path="/create-ticket" element={<TicketForm />} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/" element={<AdminDashboard />} />
           <Route path="/user-login" element={<UserLogin />} />
           <Route path="/user-register" element={<UserRegistration />} />
         </Routes>
